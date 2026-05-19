37828689e16e6132bb9bb5d4d347f4431915afe2
Corner-tnb <info@tenbestecorner.nl>
2026-05-20 01:19:01 +0200

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl =
-  "https://dcxifkkjfyeudwensnb.supabase.co";
+  "https://dcxifkkjfjyeudwensnb.supabase.co";
 
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjeGlma2tqZmp5ZXVkd2Vuc25iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyMTU2MDMsImV4cCI6MjA5NDc5MTYwM30.WrTXb4Gy9Mu2BmYY6IBabkqFbCO1T3TglHYZW-WEHgE";
