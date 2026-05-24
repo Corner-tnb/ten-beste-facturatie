389fb99a6430c3db3f4e0c94b378ea95d95150f6
Corner-tnb <info@tenbestecorner.nl>
2026-05-24 22:44:16 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1192,6 +1192,29 @@ function exporteerCSV() {
   ];
 
   alleFacturenBedrijf.forEach((f) => {
+    const csv = rows.map((r) => r.join(";")).join("\n");
+
+const blob = new Blob(["\uFEFF" + csv], {
+  type: "text/csv;charset=utf-8;",
+});
+
+const url = URL.createObjectURL(blob);
+
+const link = document.createElement("a");
+link.href = url;
+
+link.setAttribute(
+  "download",
+  `boekhouding-${bedrijf.naam}.csv`
+);
+
+document.body.appendChild(link);
+link.click();
+document.body.removeChild(link);
+
+URL.revokeObjectURL(url);
+}
+                              
     rows.push([
       f.factuurnummer,
       f.klant_naam,
